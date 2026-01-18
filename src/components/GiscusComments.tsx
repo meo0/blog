1b05b1ad4fa709be48beecfe97a2a69899f8bfd3
meo0 <hashidamemos@gmail.com>
2026-01-18 22:06:18 +0900

    fix: Update Giscus repoId and categoryId from placeholders to actual values.

--- src/components/GiscusComments.tsx
+++ src/components/GiscusComments.tsx
@@ -6,9 +6,9 @@ export default function GiscusComments() {
   return (
     <Giscus
       repo="meo0/blog"
-      repoId="REPO_ID_HERE"
+      repoId="R_kgDOQ8Il0g"
       category="General"
-      categoryId="CATEGORY_ID_HERE"
+      categoryId="DIC_kwDOQ8Il0s4C1G0l"
       mapping="pathname"
       reactionsEnabled="1"
       emitMetadata="0"
